29516f9d397d7dae7c1637c6e83b9bd87ba7fc99
Rory McStay <rory@rorymcstay.com>
2020-02-03 20:19:27 +0000

    multiple inclusion guards

--- domain/inc/Enums.h
+++ domain/inc/Enums.h
@@ -1,3 +1,6 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
 #include <include.hpp>
 
 namespace domain
@@ -6,3 +9,5 @@ namespace domain
     ENUM_MACRO_2(EventType, MarketData, TradeMessage);
     ENUM_MACRO_7(MessageType, NewOrder, Cancel, Deletion, Execution, HiddenExecution, TradingHalt, Unknown)
 }
+
+#endif // ENUMS_H
